72353b1c81961b6cac590bd778422d38570d5f23
Klesti Selimaj <klesti.selimay@icloud.com>
2026-08-05 04:55:20 +0200

    Wokring streaming

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 pub mod executor;
 
-fn main() {
-    println!("Hello, world!");
+use futures_util::{SinkExt, StreamExt};
+use serde_json::json;
+use tokio_tungstenite::connect_async;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let (mut ws, _) = connect_async("wss://pumpdev.io/ws").await?;
+
+    ws.send(tokio_tungstenite::tungstenite::Message::Text(
+        json!({ "method": "subscribeNewToken" }).to_string().into(),
+    ))
+    .await?;
+
+    while let Some(msg) = ws.next().await {
+        let msg = msg?;
+        if let tokio_tungstenite::tungstenite::Message::Text(text) = msg {
+            let token: serde_json::Value = serde_json::from_str(&text)?;
+            // println!("{} ({})", token.name, token.symbol);
+            println!("{:?}", token);
+        }
+    }
+    Ok(())
 }
